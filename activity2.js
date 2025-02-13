a71ed018ddc9a9f6c3c10a7f0ff0bc154d6dbf58
Jerome Giray <jeromegiray500@gmail.com>
2025-02-13 21:47:15 +0800

    Completed Variable section

---
+++ activity2.js
@@ -0,0 +1,9 @@
+let firstFavNum = 9;//This line uses let to declare the variable name firstFavNum which has a value of 9.
+let secondFavNum = 30;//This line uses let to declare variable name secondFavNum which has a value of 30.
+
+console.log(firstFavNum + secondFavNum);// Calculates the sum of firstFavNum and secondFavNum. Then print/show the result, which is 39.
+console.log(firstFavNum - secondFavNum);//Calculates the difference of firstFavNum and secondFavNum. Then print the result, which is -21.
+console.log(firstFavNum * secondFavNum);//Calculates the product of firstFavNum and secondFavNum. Then print the result which is 270.
+console.log(firstFavNum/secondFavNum);//Calculates the quotient of firstFavNum and secondFavNum. Then print the result which is 0.3.
+console.log(firstFavNum % secondFavNum);//This line calculates if there are remainder if we divide the value of firstFavNum to secondFavNum. Then print the result which is 9.
+console.log(firstFavNum ** secondFavNum);//This calculates if we raised the value of firstFavNum by the value of secondFavNum. Then print the result which is 4.23911582752162e+28.
