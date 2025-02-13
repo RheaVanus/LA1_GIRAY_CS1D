4d73ea801af3575befa95acbf9072666876477c0
Jerome Giray <jeromegiray500@gmail.com>
2025-02-13 14:39:20 +0800

    Completed Variables section

---
+++ activity1.js
@@ -0,0 +1,10 @@
+let name = "Jerome V. Giray";// This line declares a variable named "name" and assigns a value "Jerome V. Giray"
+let whatDoYouWannaBecomeInLife = "programmer";//This line declares a variable named "whatDoYouWannaBecomeInLife" and assigns a value "programmer" inside it.
+let gender = "male";//This line declares a variable named "gender" and assigns a value "male" inside.
+let twitterHandle = "@TrolllJerome";//This line declares a variable named "twitterHandle" and assigns a value "@TrolllJerome" inside.
+
+//These console.log are used to call the variable names: names, whatDoYouWannaBecomeInLife, gender, and twitterHandle then displat their values once the script run.
+console.log(name);
+console.log(whatDoYouWannaBecomeInLife);
+console.log(gender);
+console.log(twitterHandle);
