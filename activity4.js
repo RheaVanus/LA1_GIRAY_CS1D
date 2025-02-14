3e571a9e72c4aec323dca41afe09e9876068ecce
Jerome Giray <jeromegiray500@gmail.com>
2025-02-14 16:52:55 +0800

    Completed Relational Operators section

---
+++ activity4.js
@@ -0,0 +1,20 @@
+let firstFavNum = 9; // This line declare a variable name firstFavNum and stores my favorite number which is 9
+let secondFavNum = 30;//This ine der
+
+let greaterThan = firstFavNum > secondFavNum;//This line checks if firstFavNum is greater than secondFavNum.
+let lessThan = firstFavNum < secondFavNum;//This line checks if firstFavNum is less than secondFavNum.
+let greatOrEquals = firstFavNum >= secondFavNum;//This line checks if firstFavNum is greater than or equals to secondFavNum.
+let lessOrEqual = firstFavNum <= secondFavNum;//This line checks if firstFavNum is less than or equals to secondFavNum.
+let strictEquality= firstFavNum === secondFavNum;//This line checks if firstFavNum is greater than secondFavNum using strict equality operator.
+let looseEquality = firstFavNum == secondFavNum;//This line checks if firstFavNum is greater than secondFavNum using loose equality operator.
+let strictInequality = firstFavNum !== secondFavNum;// This line checks if firstFavNum is less than secondFavNum using strict non-equality operator.
+let looseInequality = firstFavNum != secondFavNum;// This line checks if firstFavNum is less than secondFavNum using loose non-equality operator.
+
+console.log (greaterThan);
+console.log (lessThan);
+console.log (greatOrEquals);
+console.log (lessOrEqual);
+console.log (strictEquality);
+console.log (looseEquality);
+console.log (strictInequality);
+console.log (looseInequality);
